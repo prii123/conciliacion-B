bb3cf132c23c18ac385efd784dc1a6bf596e12f1
prii123 <printsvallejos@gmail.com>
2025-12-15 19:05:40 -0500

    .

--- app/database.py
+++ app/database.py
@@ -3,14 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./conciliaciones.db")  DATABASE_URL = "postgresql://usuario:contraseña@localhost:5432/tu_basededatos"
-# DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://usuario:contraseña@localhost:5432/tu_basededatos")  
+
 
     # Cambia los valores de usuario, contraseña, host, puerto y base de datos según tu configuración
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://printsvallejos:04373847Vallejos@64.23.180.56:5432/conciliaciones"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL no está definido en el entorno. Configura tu archivo .env correctamente.")
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
